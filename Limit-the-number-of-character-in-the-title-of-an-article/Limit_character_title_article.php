745fde07eb1776d5ae0d23dfd1cbac0c7e8fab03
Spiky <lesurfeur72@gmail.com>
2014-08-29 13:32:55 +0200

    Update Limit_character_title_article.php

--- Limit-the-number-of-character-in-the-title-of-an-article/Limit_character_title_article.php
+++ Limit-the-number-of-character-in-the-title-of-an-article/Limit_character_title_article.php
@@ -8,7 +8,7 @@ Author: lesurfeur
 /*****************
 **  UTILISATION **
 ******************
-Replace in (Remplacer dans) content.php :
+Replace in your theme (Remplacer dans) content.php :
 <?php if ( is_single() ) {
 
 by (par) 
@@ -17,11 +17,11 @@ by (par)
 
 Replace (remplacer) :
 
-<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( esc_html__( 'Permalink to %s', 'quark' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
+<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( esc_html__( 'Permalink to %s', 'Your_Theme' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
 
 by (par)
 
-<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( esc_html__( 'Permalink to %s', 'quark' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php shortened_title(); ?></a>
+<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( esc_html__( 'Permalink to %s', 'Your_Theme' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php shortened_title(); ?></a>
 
 ******************
 ******************/

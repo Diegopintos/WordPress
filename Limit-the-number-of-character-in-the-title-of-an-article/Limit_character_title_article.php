3a4d4522190bffc6453522e71ed2d953d9e63f29
Spiky <lesurfeur72@gmail.com>
2014-08-29 13:37:10 +0200

    Update Limit_character_title_article.php

--- Limit-the-number-of-character-in-the-title-of-an-article/Limit_character_title_article.php
+++ Limit-the-number-of-character-in-the-title-of-an-article/Limit_character_title_article.php
@@ -10,25 +10,24 @@
  */
 
 /*****************
-**  UTILISATION **
-******************
-Replace in your theme (Remplacer dans) content.php :
-<?php if ( is_single() ) {
+ **  UTILISATION **
+ ******************
+ Replace in your theme (Remplacer dans) content.php :
+ <?php if ( is_single() ) {
 
-by (par) 
+ by (par) 
 
-<?php if ( is_single() || is_search() ) { ?>
+ <?php if ( is_single() || is_search() ) { ?>
 
-Replace (remplacer) :
+ Replace (remplacer) :
 
-<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( esc_html__( 'Permalink to %s', 'Your_Theme' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
+ <a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( esc_html__( 'Permalink to %s', 'Your_Theme' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
 
-by (par)
+ by (par)
 
-<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( esc_html__( 'Permalink to %s', 'Your_Theme' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php shortened_title(); ?></a>
+ <a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( esc_html__( 'Permalink to %s', 'Your_Theme' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php shortened_title(); ?></a>
 
-******************
-******************/
+ ******************/
 
 function shortened_title() {
 

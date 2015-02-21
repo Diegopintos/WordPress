ff5ac65e61acef822515e2f18122b999d02b1a22
Spiky <lesurfeur72@gmail.com>
2015-02-21 22:49:47 +0100

    Update functions.php

--- Theme/Child-Theme/MovieLibrary/functions.php
+++ Theme/Child-Theme/MovieLibrary/functions.php
@@ -1,6 +1,6 @@
 <?php
 /* ------------------------------------------------------------------------- *
-* Custom functions
+ * Custom functions
 /* ------------------------------------------------------------------------- */
 // Add your custom functions here, or overwrite existing ones. Read more how to use:
 // http://codex.wordpress.org/Child_Themes
